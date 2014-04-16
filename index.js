ba185cd9eeb12dd702965cb1309a0e9bb9eda8f1
braun <braun@brauns-iMac.local>
2014-04-15 21:58:28 -0400

    ok

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log('facebyte');
 
 // CRYPTOGRAPHY //
-var cryptography = require("crypto");
+var cryptography = require('crypto');
+var atob = require('atob');
+var btoa = require('btoa');
 
 // MONGODB //
 var mongodb = require('mongodb');
@@ -10,7 +12,7 @@ var database = null;
 
 mongoClient.connect('mongodb://localhost:20000/facebyte',mongodbClientConnect_handler);
 
-function mongodbClientConnect_handler (error,_database){
+function mongodbClientConnect_handler (error,_database) {
 	if (error) {
 		throw error;
 	} else {
@@ -22,15 +24,30 @@ function mongodbClientConnect_handler (error,_database){
 var express = require('express.io');
 var http = express();
 
-http.http();
+http.http().io();
+http.use(express.logger());
+http.use('/',express.static(__dirname+'/public'));
 http.listen(process.env.PORT||30000);
-
 http.get('/users',httpGet_users);
 http.post('/users/:username/:password',httpPost_usersCreate);
 http.post('/users/tokens/:username/:password',httpPost_usersTokensCreate);
-http.post('/users/photos/:facebyte/:token',httpPost_usersPhotos);
+http.post('/users/:userame/:token/:facebyte',httpPost_usersPhotosUpdate);
 
 // CLASSES //
+
+function User (user) {
+	var _user = user || {};
+	this.username = _user.username || btoa("Guest");
+	this.password = _user.password || btoa("Password");
+	this.token = _user.token || btoa(new Token()());
+	this.facebyte = _user.facebyte || btoa("");
+	this.dates = _user.dates || {
+		created: btoa(new Date()),
+		lastUpdated: btoa(new Date())
+	};
+	this.update = _user.update || btoa("Hey, facebytes! Care to welcome @"+atob(this.username)+" to facebyte? :)");
+}
+
 function httpGet_users (request,response) {
 	if (database) {
 		var users = database.collection('users');
@@ -48,31 +65,34 @@ function httpGet_users (request,response) {
 function httpPost_usersCreate (request,response) {
 	if (database) {
 		var users = database.collection('users');
-		var user = {
-			username: request.params.username,
-			password: request.params.password
-		};
-		users.insert(user,function usersInsert_callback (error,objects) {
-			if (error) {
-				console.warn(error.message);
-				response.send('{"error":"username already exists."}');
-			} else {
-				var token = new Token()();
-				users.update(
-					{username:user.username},
-					{$set:{token:token}},
-					function usersUpdate_tokenCallback (error,objects) {
-						console.log(objects);
-						if (error) {
-							console.warn(error.message);
-							response.send('{"error":"not authorized"}');
-						} else {
-							response.send(JSON.stringify({token:token}));
+		var user = new User(request.params);
+		if (user.username.length > 4) {
+			users.insert(user,function usersInsert_callback (error,objects) {
+				if (error) {
+					console.warn(error.message);
+					response.send('{"error":"username already exists."}');
+				} else {
+					var token = new Token()();
+					users.update(
+						{username:user.username},
+						{$set:{token:token}},
+						function usersUpdate_tokenCallback (error,objects) {
+							console.log(objects);
+							if (error) {
+								console.warn(error.message);
+								response.send('{"error":"not authorized"}');
+							} else {
+								response.send(JSON.stringify({token:token}));
+							}
 						}
-					}
-				);
-			}
-		});
+					);
+				}
+			});
+		} else {
+			response.send('{"error":"not authorized: username must contain at least four characters"}');
+		}
+	} else {
+		response.send('{"error":"not authorized: database"}');
 	}
 }
 
@@ -103,12 +123,13 @@ function httpPost_usersTokensCreate (request,response) {
 	}
 }
 
-function httpPost_usersPhotos (request,response) {
+function httpPost_usersPhotosUpdate (request,response) {
 	if (database) {
 		var users = database.collection('users');
 		var user = {
-			token: request.params.token,
-			facebyte: request.params.facebyte
+			username: request.params.username.atob(),
+			token: request.params.token.atob(),
+			facebyte: request.params.facebyte.atob()
 		};
 		users.update(
 			{token:user.token},
